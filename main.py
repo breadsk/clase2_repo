db5f173dcbd2ed84921aa549cf35e39cd2ca43ad
Nicolas <nicolas.programador@gmail.com>
2025-06-13 15:30:10 -0400

    Funcionalidad suma ok

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-print("Hola 1")
-print("Hola 2")
-print("Hola 3")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
+
+
+print("Calculadora")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Salir")
+
+resultado = 0
+
+while True:
+    opcion = int(input("Ingrese la opcion: "))
+    num1 = int(input("Ingrese numero1: "))
+    num2 = int(input("Ingrese numero2: "))
+    if opcion == 1:
+        resultado = num1 + num2
+        print(f"El resultado es: {resultado}")
+    elif opcion == 5:
+        break
+
+print("Gracias")
